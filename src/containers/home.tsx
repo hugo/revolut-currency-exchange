6dd11c985551f9106d464de8b5569ad11effff52
Hugo Jobling <hello@thisishugo.com>
2020-07-10 11:30:18 +0100

    Move home page content to `containers` directory
    Due to quirks of how Next discovers pages, we don't want tests in the
    `pages` directory. However, it is my strong preference to colocate tests
    with the files they test. Therefore, I'm using the `containers` folder
    to actually put pages in. The files in `pages` will be a mirrored
    structure of `containers`.

---
+++ src/containers/home.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Exchange</title>
+      </Head>
+
+      <div>
+        <h1>Exchange</h1>
+      </div>
+    </>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,1 @@
-import Head from 'next/head'
-
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Exchange</title>
-      </Head>
-
-      <div>
-        <h1>Exchange</h1>
-      </div>
-    </>
-  )
-}
+export {default} from './containers/home'
